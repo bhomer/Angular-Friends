faf49a461f14e17e4eb47be546cf5d520ea3ffa9
Benjamin <benjaminhomer@gmail.com>
2014-01-31 19:32:30 -0700

    updated through step 5

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = angular.module('AngularFriends',[]);
+
+console.log("page loaded");
+//$.get(https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json");
+//      });
+
+console.log("hello");
